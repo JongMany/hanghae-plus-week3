4275f6ae527852952f5cc0900f32af4b03c69967
JongMany <blackberry1114@naver.com>
2024-03-25 16:01:51 +0900

    과제2 해결

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,44 @@
-export const 구독 = fn => {
-  fn();
-}
+const publisher = {
+  state: null,
+  subscribers: new Map(),
+  alert(prop) {
+    console.log(this.subscribers);
+    this.subscribers[prop]?.forEach((fn) => {
+      fn();
+    });
+  },
+};
 
-export const 발행기관 = obj => {
-  return obj;
-}
+let currentCallback = null;
+export const 구독 = (fn) => {
+  currentCallback = fn;
+
+  fn();
+};
+
+export const 발행기관 = (initState) => {
+  publisher.subscribers = Object.keys(initState).reduce((map, key) => {
+    map[key] = new Set();
+    return map;
+  }, {});
+
+  publisher.state = new Proxy(initState, {
+    get: (obj, prop) => {
+      if (currentCallback) {
+        publisher.subscribers[prop].add(currentCallback);
+      }
+
+      return obj[prop];
+    },
+    set: (obj, prop, value) => {
+      if (obj[prop] === value) return true;
+      if (JSON.stringify(obj[prop]) === JSON.stringify(value)) return true;
+      console.log(obj, prop, value);
+      obj[prop] = value;
+      publisher.alert(prop);
+      return true;
+    },
+  });
+  currentCallback = null;
+  return publisher.state;
+};
